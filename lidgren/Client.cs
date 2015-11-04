855db86489b7b30bef026d56a38dde2a5477c11c
Jake Chiang <jakewchiang@gmail.com>
2015-11-03 18:39:16 -0800

    Initial Lidgren network code.

---
+++ lidgren/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Lidgren.Network;
+
+namespace XamarinTesting {
+    public class Client {
+        static public void Main(string[] args) {
+            Console.WriteLine("[CLIENT] Testing Lidgren-Network-v3...");
+
+            NetPeerConfiguration clientConfig = new NetPeerConfiguration("test");
+            clientConfig.EnableMessageType(NetIncomingMessageType.DiscoveryResponse);
+            NetClient client = new NetClient(clientConfig);
+            client.Start();
+
+            Console.WriteLine("IP to connect to: ");
+            String ip = Console.ReadLine();
+            client.Connect(ip, 12345);
+
+            while(true) {
+                if(client.ServerConnection != null) {
+                    string msg = Console.ReadLine();
+
+                    NetOutgoingMessage sendMsg = client.CreateMessage();
+                    sendMsg.Write(msg);
+                    client.SendMessage(sendMsg, NetDeliveryMethod.ReliableOrdered);
+                }
+            }
+        }
+    }
+}
+
---
+++ lidgren/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Lidgren.Network;
+
+namespace XamarinTesting {
+    class MainClass {
+        public static void Main(string[] args) {
+            Console.WriteLine("[SERVER] Testing Lidgren-Network-v3...");
+
+            NetPeerConfiguration serverConfig = new NetPeerConfiguration("test");
+            serverConfig.EnableMessageType(NetIncomingMessageType.DiscoveryRequest);
+            serverConfig.MaximumConnections = 32;
+            serverConfig.Port = 12345;
+            NetServer server = new NetServer(serverConfig);
+            server.Start();
+
+            while(true) {
+                NetIncomingMessage inc;
+                while((inc = server.ReadMessage()) != null) {
+                    Console.WriteLine(inc.ReadString());
+
+//                    switch(inc.MessageType) {
+//                    case NetIncomingMessageType.DiscoveryRequest:
+//
+//                    // Create a response and write some example data to it
+//                        NetOutgoingMessage response = server.CreateMessage();
+//                        response.Write("My server name");
+//
+//                    // Send the response to the sender of the request
+//                        server.SendDiscoveryResponse(response, inc.SenderEndPoint);
+//                        break;
+//                    }
+                }
+            }
+        }
+    }
+}

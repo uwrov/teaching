3cee597c7a96bfdd161322719610dbf60299c170
Jake Chiang <jakewchiang@gmail.com>
2015-11-05 19:35:28 -0800

    Added two-way chat program demo

---
+++ lidgren-chat/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using Lidgren.Network;
+
+namespace NetworkTesting {
+    public class Client {
+        public static void GotMessage(object obj) {
+            NetClient peer = (NetClient)obj;
+            var msg = peer.ReadMessage();
+
+            Console.WriteLine(msg.ReadString());
+        }
+
+        static public void Main(string[] args) {
+            Console.WriteLine("[CLIENT] Testing Lidgren-Network-v3...");
+
+            NetPeerConfiguration clientConfig = new NetPeerConfiguration("test");
+            clientConfig.EnableMessageType(NetIncomingMessageType.DiscoveryResponse);
+            NetClient client = new NetClient(clientConfig);
+            client.Start();
+
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+            client.RegisterReceivedCallback(new SendOrPostCallback(GotMessage));
+
+            Console.WriteLine("IP to connect to: ");
+            String ip = Console.ReadLine();
+
+            client.Connect(new System.Net.IPEndPoint(System.Net.IPAddress.Parse(ip), 12345));
+
+            while(true) {
+                string msg = Console.ReadLine();
+
+                NetOutgoingMessage sendMsg = client.CreateMessage();
+                sendMsg.Write("[CLIENT] " + msg);
+                client.SendMessage(sendMsg, NetDeliveryMethod.ReliableOrdered);
+            }
+        }
+    }
+}
+
---
+++ lidgren-chat/Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using Lidgren.Network;
+
+namespace NetworkTesting {
+    class Server {
+        public static void GotMessage(object obj) {
+            NetServer peer = (NetServer)obj;
+            var msg = peer.ReadMessage();
+
+            Console.WriteLine(msg.ReadString());
+        }
+
+        public static void Main(string[] args) {
+            Console.WriteLine("[SERVER] Testing Lidgren-Network-v3...");
+            NetPeerConfiguration serverConfig = new NetPeerConfiguration("test");
+            serverConfig.EnableMessageType(NetIncomingMessageType.DiscoveryRequest);
+            serverConfig.MaximumConnections = 32;
+            serverConfig.Port = 12345;
+            NetServer server = new NetServer(serverConfig);
+            server.Start();
+
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+            server.RegisterReceivedCallback(new SendOrPostCallback(GotMessage));
+
+            while(true) {
+                string msg = Console.ReadLine();
+
+                NetOutgoingMessage sendMsg = server.CreateMessage();
+                sendMsg.Write("[SERVER] " + msg);
+
+                server.SendToAll(sendMsg, NetDeliveryMethod.ReliableOrdered);
+            }
+        }
+    }
+}
